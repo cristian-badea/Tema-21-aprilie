beec245e069b54382f88fb99065f908ac563f9a7
Cristian Alexandru Victor Badea <badea_cristian_alexandru@yahoo.com>
2015-04-21 23:07:33 +0300

    Tema NumarText 21 aprilie

---
+++ NumberToText.App/FormNumberToText.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace NumberToText.App
+{
+    partial class FormNumberToText
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.labelText = new System.Windows.Forms.Label();
+            this.comboBoxLanguage = new System.Windows.Forms.ComboBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(30, 31);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumber.TabIndex = 0;
+            this.textBoxNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxNumber_KeyPress);
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Location = new System.Drawing.Point(162, 34);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(0, 13);
+            this.labelText.TabIndex = 1;
+            // 
+            // comboBoxLanguage
+            // 
+            this.comboBoxLanguage.FormattingEnabled = true;
+            this.comboBoxLanguage.Location = new System.Drawing.Point(30, 86);
+            this.comboBoxLanguage.Name = "comboBoxLanguage";
+            this.comboBoxLanguage.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxLanguage.TabIndex = 2;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(165, 84);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 3;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // FormNumberToText
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 136);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.comboBoxLanguage);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.textBoxNumber);
+            this.Name = "FormNumberToText";
+            this.Text = "Number to Text";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.ComboBox comboBoxLanguage;
+        private System.Windows.Forms.Button buttonConvert;
+    }
+}
+
---
+++ NumberToText.App/FormNumberToText.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Humanizer;
+using System.Globalization;
+
+namespace NumberToText.App
+{
+    public partial class FormNumberToText : Form
+    {
+
+        public FormNumberToText()
+        {
+            InitializeComponent();
+            Dictionary<string, string> _comboBoxDictionary = new Dictionary<string, string>();
+            _comboBoxDictionary.Add("en", "English");
+            _comboBoxDictionary.Add("fr", "French");
+            _comboBoxDictionary.Add("it", "Italian");
+            _comboBoxDictionary.Add("de", "German");
+
+            comboBoxLanguage.DataSource = new BindingSource(_comboBoxDictionary, null);
+            comboBoxLanguage.DisplayMember = "Value";
+            comboBoxLanguage.ValueMember = "Key";
+
+            comboBoxLanguage.SelectedIndex = 0;
+        }
+
+        private void textBoxNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char character = e.KeyChar;
+            if (Char.IsDigit(character) == false && character != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show("Only numbers!");
+            }
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            if (textBoxNumber.Text != "")
+            {
+                labelText.Text = int.Parse(textBoxNumber.Text).ToWords(new CultureInfo(((KeyValuePair<string, string>)comboBoxLanguage.SelectedItem).Key));
+                
+            }
+            else
+            {
+                MessageBox.Show("Please insert a number first!");
+            }
+        }
+    }
+}
